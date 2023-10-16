31b72280f4c551d192a09bb708f39dc882de66e4
Sam <sampharris2000@hotmail.co.uk>
2023-10-16 15:46:02 +0100

    Change Auth Address Again

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,5 +4,5 @@ export const DEVELOPMENT_AUTH_SERVER = "http://localhost:3000/";
 export const PRODUCTION_APP_SERVER = "https://stint-writer-app.vercel.app/";
 export const PRODUCTION_AUTH_SERVER = "https://stint-writer-auth.vercel.app";
 
-export const CURRENT_AUTH_SERVER = DEVELOPMENT_AUTH_SERVER;
+export const CURRENT_AUTH_SERVER = PRODUCTION_AUTH_SERVER;
 export const CURRENT_APP_SERVER = DEVELOPMENT_APP_SERVER;
